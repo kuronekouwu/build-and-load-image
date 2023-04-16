57181af79980c80d78499d81657612180abe3e64
M307 (Mac) <me@m307.dev>
2023-04-16 11:21:37 +0700

    chroic: Hm2

--- dist/index.js
+++ dist/index.js
@@ -9799,31 +9799,56 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
-const cp = __nccwpck_require__(2081)
+const cp = __nccwpck_require__(2081);
 
-
-
+const cpCfg = {
+  stdio: "inherit",
+  maxBuffer: 50 * 1024 * 1024,
+};
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  // Get config
+  const imageRef = core.getInput("image", { required: false });
+  const imageTag = core.getInput("version",  { required: false });
+  const tcpURL = core.getInput("url", { required: true });
 
-  // Test docker version
-  console.log("DOCKER VERSION IS")
-  cp.execSync('docker info', {
-    stdio: 'inherit',
-    maxBuffer: 50 * 1024 * 1024
-  })
+  // Get docker info
+  cp.execSync("docker info", cpCfg);
+  // Create image
+  let imageName = "";
+  if (!imageRef) {
+    imageName += `${github.context.payload.repository.full_name.replace(
+      "/",
+      "-"
+    )}-${github.context.sha.substring(0, 7)}:${imageTag || 'latest'}`;
+  }else{
+    imageName += `${imageRef}:${imageTag || 'latest'}`
+  }
+  // Build image command
+  console.log('Your image: ' + imageName)
+//   cp.execSync(`docker build `)
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-//   const payload = JSON.stringify(github.context.payload, undefined, 2)
-//   console.log(`The event payload: ${payload}`);
+
+  //   // `who-to-greet` input defined in action metadata file
+  //   const nameToGreet = core.getInput("who-to-greet");
+  //   console.log(`Hello ${nameToGreet}!`);
+  //   const time = new Date().toTimeString();
+  //   core.setOutput("time", time);
+
+  //   // Test docker version
+  //   console.log("DOCKER VERSION IS");
+  //   cp.execSync("docker info", {
+  //     stdio: "inherit",
+  //     maxBuffer: 50 * 1024 * 1024,
+  //   });
+
+  //   // Get the JSON webhook payload for the event that triggered the workflow
+  //   //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
+
 })();
 
 module.exports = __webpack_exports__;
