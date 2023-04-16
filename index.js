587c19f334f7af0947948c0d1f5a47917579e620
M307 (Mac) <me@m307.dev>
2023-04-16 22:09:29 +0700

    feat: Change sync to async

--- index.js
+++ index.js
@@ -6,50 +6,59 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const cp = require("child_process");
-const fs = require('fs');
+const fs = require("fs");
 
 const cpCfg = {
   stdio: "inherit",
   maxBuffer: 50 * 1024 * 1024,
 };
 
-try {
-  // Get config
-  const imageRef = core.getInput("image", { required: false });
-  const imageTag = core.getInput("version",  { required: false });
-  const dkFile = core.getInput('dockerfile', { required: false })
-  const tcpURL = core.getInput("url", { required: true });
+(async () => {
+  try {
+    // Get config
+    const imageRef = core.getInput("image", { required: false });
+    const imageTag = core.getInput("version", { required: false });
+    const dkFile = core.getInput("dockerfile", { required: false });
+    const tcpURL = core.getInput("url", { required: true });
 
-  // Get docker info
-  cp.execSync("docker info", cpCfg);
-  // Get short sha 
-  const sha = github.context.sha.substring(0, 7)
+    // Get docker info
+    await cp.execSync("docker info", cpCfg);
+    // Get short sha
+    const sha = github.context.sha.substring(0, 7);
 
-  // Create image
-  let imageName = "";
-  if (!imageRef) {
-    imageName += `${github.context.payload.repository.full_name.replace(
-      "/",
-      "-"
-    )}-${sha}:${imageTag || 'latest'}`;
-  }else{
-    imageName += `${imageRef}:${imageTag || 'latest'}`
+    // Create image
+    let imageName = "";
+    if (!imageRef) {
+      imageName += `${github.context.payload.repository.full_name.replace(
+        "/",
+        "-"
+      )}-${sha}:${imageTag || "latest"}`;
+    } else {
+      imageName += `${imageRef}:${imageTag || "latest"}`;
+    }
+    // Check file is not exists
+    if (!fs.existsSync(dkFile))
+      throw new Error(
+        "File " +
+          dkFile +
+          " not found. Please check your directory dockerfile is exist"
+      );
+
+    // Build it
+    const fileExportName = `${sha}.tar`;
+    console.log("⚙️ Building to image");
+    await cp.execSync(`docker build -t ${imageName} ${dkFile}`, cpCfg);
+    // Save image
+    console.log("📝 Export to .tar file");
+    await cp.execSync(`docker save ${imageName} > ${fileExportName}`, cpCfg);
+    // Uploading to server
+    console.log("📈 Uploading to server...");
+    await cp.execSync(
+      `curl -X POST '${tcpURL}/images/load' -H 'Content-Type: application/x-tar'  --data-binary "@${fileExportName}"`,
+      cpCfg
+    );
+    console.log(`✅ N O I C E`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  // Check file is not exists
-  if(!fs.existsSync(dkFile)) throw new Error('File ' + dkFile + ' not found. Please check your directory dockerfile is exist')
-
-  // Build it
-  const fileExportName = `${sha}.tar`
-  console.log('⚙️ Building to image')
-  cp.execSync(`docker build -t ${imageName} ${dkFile}`, cpCfg)
-  // Save image
-  console.log('📝 Export to .tar file')
-  cp.execSync(`docker save ${imageName} > ${fileExportName}`, cpCfg)
-  // Uploading to server
-  console.log('📈 Uploading to server...')
-  cp.execSync(`curl -X POST '${tcpURL}/images/load' -H 'Content-Type: application/x-tar'  --data-binary "@${fileExportName}"`, cpCfg)
-  console.log(`✅ N O I C E`)
-
-} catch (error) {
-  core.setFailed(error.message);
-}
+})();
