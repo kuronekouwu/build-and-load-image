23062768fdab8a2e4ab4a05f6ba4e044f71d6742
M307 (Mac) <me@m307.dev>
2023-04-16 11:40:07 +0700

    feat: Try it!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const cp = require("child_process");
+const fs = require('fs');
+const axios = require('axios');
 
 const cpCfg = {
   stdio: "inherit",
@@ -9,24 +11,40 @@ const cpCfg = {
 
 try {
   // Get config
-  const imageRef = core.getInput("image", { required: false });
-  const imageTag = core.getInput("version",  { required: false });
+  const imageRef = core.getInput("image", { required: true });
+  const imageTag = core.getInput("version",  { required: true });
+  const dkFile = core.getInput('dockerfile', { required: true })
   const tcpURL = core.getInput("url", { required: true });
 
   // Get docker info
   cp.execSync("docker info", cpCfg);
+  // Get short sha 
+  const sha = github.context.sha.substring(0, 7)
+
   // Create image
   let imageName = "";
   if (!imageRef) {
     imageName += `${github.context.payload.repository.full_name.replace(
       "/",
       "-"
-    )}-${github.context.sha.substring(0, 7)}:${imageTag || 'latest'}`;
+    )}-${sha}:${imageTag || 'latest'}`;
   }else{
     imageName += `${imageRef}:${imageTag || 'latest'}`
   }
-  // Build image command
-  console.log('Your image: ' + imageName)
+  // Check file is not exists
+  if(!fs.existsSync(dkFile)) throw new Error('File ' + dkFile + ' not found. Please check your directory dockerfile is exist')
+
+  // Build it
+  const fileExportName = `${sha}.tar`
+  console.log('⚙️ Building to image')
+  cp.execSync(`docker build -t ${imageName} ${dkFile}`)
+  // Save image
+  console.log('📝 Export to .tar file')
+  cp.execSync(`docker save ${imageName} > ${fileExportName}`)
+  // Uploading to server
+  console.log('📈 Uploading to server...')
+  cp.execSync(`curl -X POST '${tcpURL}/images/load' -H 'Content-Type: application/x-tar'  --data-binary "@${fileExportName}"`)
+  console.log(`✅ N O I C E`)
 
 } catch (error) {
   core.setFailed(error.message);
